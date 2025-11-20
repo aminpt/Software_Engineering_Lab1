22dded86841da179117d54f5218ecc2f46664333
davouddkareshki <ostad1kareshki@gmail.com>
2025-11-20 17:30:25 +0330

    support the styling and clicking in TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-export default function TodoList({ todos, removeTodo }) {
+export default function TodoList({ todos, removeTodo, toggleTodo }) {
   return (
     <ul className="todo-list">
       {todos.map(todo => (
-        <li key={todo.id}>
-          {todo.text}
+        <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+          <span onClick={() => toggleTodo(todo.id)} style={{cursor: 'pointer', flex: 1}}>
+            {todo.completed ? '✅ ' : 'Dw '} {}
+            {todo.text}
+          </span>
           <button onClick={() => removeTodo(todo.id)}>❌</button>
         </li>
       ))}
